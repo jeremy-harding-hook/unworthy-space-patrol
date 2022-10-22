a1986e6d280548f8408ad18b67a79b47e9e4f4a3
Jeremy Harding Hook <jeremyhhook@gmail.com>
2022-10-22 19:44:15 +0200

    implemented basic movement of character. Needed is a way to make the movement more fluid, rather than relying on keydowns. Maybe a timer loop with a check? Also, need to hide the cursor.

--- src/main.c
+++ src/main.c
@@ -4,23 +4,52 @@
 #include <locale.h>
 #include "stringing.h"
 
+#define increment_within_bounds(x,y) (x == (y) ? x = 0 : x++)
+#define decrement_within_bounds(x,y) (x == 0 ? x = (y) : x--)
+#define LAST_COL ((unsigned char)(COLS-1))
+#define LAST_LINE ((unsigned char)(LINES-1))
 int main(void){
 	unsigned char centre_x;
 	unsigned char centre_y;
 	printf("Starting...\n");
-	
+
 	/* initialise ncurses */
 	setlocale(LC_ALL, "");
 	initscr();
 	cbreak();
 	noecho();
+	keypad(stdscr, TRUE);
 	clear();
 
-	centre_x = (unsigned char)(COLS-1)/2;
-	centre_y = (unsigned char)(LINES-1)/2;
+	centre_x = LAST_COL/2;
+	centre_y = LAST_LINE/2;
 	mvaddch(centre_y, centre_x, '#');
 	refresh();
-	sleep(3);
+	while(true){
+		register int input_char = getch();
+		mvaddch(centre_y, centre_x, ' ');
+		switch (input_char)
+		{
+			case KEY_LEFT:
+				decrement_within_bounds(centre_x, LAST_COL);
+				break;
+			case KEY_RIGHT:
+				increment_within_bounds(centre_x, LAST_COL);
+				break;
+			case KEY_UP:
+				decrement_within_bounds(centre_y, LAST_LINE);
+				break;
+			case KEY_DOWN:
+				increment_within_bounds(centre_y, LAST_LINE);
+				break;
+			case 'q':
+			case 'Q':
+				goto QUIT;
+		}
+		mvaddch(centre_y, centre_x, '#');
+		refresh();
+	}
+QUIT:
 	endwin();
 	printf("Exiting\n");
 	return 0;
